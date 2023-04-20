f57c2ca8796cf17b75ad47f63e698f9a2884467b
Dmitriy Mysiagin <dmitry.mysyagin@yandex.ru>
2023-04-20 08:42:29 +0300

    first structure constructor

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -10,32 +10,45 @@ import styles from "../burger-constructor/burger-constructor.module.css";
 function BurgerConstructor() {
   return (
     <>
-      <div className={styles.burger}>
-        <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
-          <DragIcon />
-          <ConstructorElement
-            type="top"
-            isLocked={true}
-            text="Краторная булка N-200i (верх)"
-            price={200}
-            thumbnail={"img"}
-          />
-          <ConstructorElement
-            text="Краторная булка N-200i (верх)"
-            price={50}
-            thumbnail={"img"}
-          />
-          <ConstructorElement
-            type="bottom"
-            isLocked={true}
-            text="Краторная булка N-200i (низ)"
-            price={200}
-            thumbnail={"img"}
-          />
+      <div className={`mt-25`}>
+        <div className={`${styles.burgerConstructor} `}>
+          <div className={`${styles.burgerConstructor__element} `}>
+            <DragIcon />
+            <ConstructorElement
+              type="top"
+              isLocked={true}
+              text="Краторная булка N-200i (верх)"
+              price={200}
+              thumbnail={"img"}
+            />
+          </div>
+          <div className={`${styles.burgerConstructor__element} `}>
+            <ConstructorElement
+              text="Краторная булка N-200i (верх)"
+              price={50}
+              thumbnail={"img"}
+            />
+          </div>
+          <div className={`${styles.burgerConstructor__element} `}>
+            <ConstructorElement
+              type="bottom"
+              isLocked={true}
+              text="Краторная булка N-200i (низ)"
+              price={200}
+              thumbnail={"img"}
+            />
+          </div>
         </div>
-        <CurrencyIcon />
-        <Button htmlType="submit" type="primary" size="large" />
+        <div className={`${styles.buttonContainer} mt-10`}>
+          <span className={`text text_type_digits-medium mr-2`}>450</span>
+          <CurrencyIcon />
+          <Button htmlType="submit" type="primary" size="large" extraClass="ml-10">
+            Оформить заказ
+          </Button>
+        </div>
       </div>
+
+      <div></div>
     </>
   );
 }
