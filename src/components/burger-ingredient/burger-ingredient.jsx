36cdfde6e1fa4c9deeae49ce90048d1f390b3531
Dmitriy Mysiagin <dmitry.mysyagin@yandex.ru>
2023-04-25 09:28:36 +0300

    first details modal, refactor ingredients

--- src/components/burger-ingredient/burger-ingredient.jsx
+++ src/components/burger-ingredient/burger-ingredient.jsx
@@ -5,33 +5,26 @@ import {
 import { useState, useEffect } from "react";
 import styles from "../burger-ingredient/burger-ingredient.module.css";
 
-function BurgerIngredient({ title, ingredients }) {
-
+function BurgerIngredient({ title, ingredient, setIngredientModal}) {
+  const handleClick = () => {
+    setIngredientModal(ingredient)
+  }
   return (
-    <>
-      <h2 className={`text text_type_main-medium pb-6`}>{title}</h2>
-      <div className={`${styles.ingredientbox} pb-10`}>
-        {ingredients
-          ? ingredients.map((ingredient) => {
-              return (
-                <div className={`${styles.ingredient} pb-8`} key={ingredient._id}>
-                  <Counter count={"0"}/>
-                  <img
-                    className={styles.ingredient__image}
-                    src={ingredient.image}
-                    alt={ingredient}
-                  ></img>
-                  <div className={`${styles.ingredient__price} pb-1 pt-1`}>
-                    <span className={`pb-1 pt-1 pr-2`}>{ingredient.price}</span>
-                    <CurrencyIcon />
-                  </div>
-                  <h4 className={`${styles.ingredient__name} text text_type_main-default`} >{ingredient.name}</h4>
-                </div>
-              );
-            })
-          : ""}
+    <div className={`${styles.ingredient} pb-8`} key={ingredient._id} onClick={handleClick}>
+      <Counter count={"0"} />
+      <img
+        className={styles.ingredient__image}
+        src={ingredient.image}
+        alt={ingredient}
+      ></img>
+      <div className={`${styles.ingredient__price} pb-1 pt-1`}>
+        <span className={`pb-1 pt-1 pr-2`}>{ingredient.price}</span>
+        <CurrencyIcon />
       </div>
-    </>
+      <h4 className={`${styles.ingredient__name} text text_type_main-default`}>
+        {ingredient.name}
+      </h4>
+    </div>
   );
 }
 
